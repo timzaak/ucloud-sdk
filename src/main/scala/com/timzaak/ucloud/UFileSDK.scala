10efa69d075579fbd8b6c61b1876a733b5f0e42e
timzaak <zsy.evan@gmail.com>
2018-11-25 00:34:56 +0800

    add privte download file api and format

--- src/main/scala/com/timzaak/ucloud/UFileSDK.scala
+++ src/main/scala/com/timzaak/ucloud/UFileSDK.scala
@@ -1,49 +1,82 @@
 package com.timzaak.ucloud
 
 import com.tizaak.ucloud.codec.HmacSHA1
-import scalaj.http.{Http, HttpConstants}
+import scalaj.http.{ Http, HttpConstants }
 
 case class UFileRequest(
-                         key: String,
-                         http_method: String = "PUT",
-                         content_md5: String = "",
-                         content_type: String = "",
-                         date: String = "",
-                         put_policy: String = ""
-                       )
+    key: String,
+    http_method: String = "PUT",
+    content_md5: String = "",
+    content_type: String = "",
+    date: String = "",
+    put_policy: String = "",
+    expires: String = ""
+)
 
 trait UFileSDK {
-  val bucket:String
-  val privateKey:String
-  val publicKey:String
-  val hostPrefix:String = "cn-sh2.ufileos.com"
+  val bucket: String
+  val privateKey: String
+  val publicKey: String
+  val hostPrefix: String = "cn-sh2.ufileos.com"
   val connTimeoutMS = 2000
   val readTimeoutMs = 20000
+  val defaultExpireTime = 60 * 30 // integer类型, 下载链接有效时间，单位为秒
   lazy val requestUrl = s"https://$bucket.$hostPrefix"
 
-  def authorization(req:UFileRequest) = {
+  private def encodeUrl(value: String) = HttpConstants.urlEncode(value, HttpConstants.utf8)
+  def authorization(req: UFileRequest) = {
     import req._
     val canonicalizedResource = s"""/$bucket/$key"""
-    val strTosig = s"${http_method.toUpperCase}\n$content_md5\n$content_type\n$date\n$canonicalizedResource"
+    val strTosig = s"${http_method.toUpperCase}\n$content_md5\n$content_type\ndate\n$canonicalizedResource"
     val signature = new HmacSHA1().sign(privateKey, strTosig)
     s"UCloud $publicKey:$signature"
   }
 
-  def putFile(param:UFileRequest,data:Array[Byte]) = {
+  def putFile(param: UFileRequest, data: Array[Byte]) = {
     import param._
-    val encodedKey = HttpConstants.urlEncode(key, "UTF-8")
-    var req = Http(s"$requestUrl/$encodedKey")
+    var req = Http(s"$requestUrl/${encodeUrl(key)}")
       .header("Authorization", authorization(param))
-      .timeout(connTimeoutMS,readTimeoutMs)
-    if(content_md5 != ""){
+      .timeout(connTimeoutMS, readTimeoutMs)
+    if (content_md5 != "") {
       req = req.header("Content-Type", content_type)
     }
-    if(content_type != ""){
+    if (content_type != "") {
       req = req.header("Content-MD5", content_md5)
     }
     if (date != "") {
       req = req.header("Date", date)
     }
+
     req.put(data).asString
+  }
+
+  def publicDownloadFileUrl(key: String, url: String = requestUrl): String = {
+    s"$url/${encodeUrl(key)}"
+  }
+
+  //私有文件还需要 User-Agent 签名，真是秀逗了。浏览器 User-Agent 贼长！
+  def privateDownloadFileUrl(
+      key: String,
+      expire: Int = defaultExpireTime,
+      userAgent: String = "scalaj4",
+      url: String = requestUrl
+  ): String = {
+    val expireStr = (System.currentTimeMillis() / 1000 + expire).toString
+    def signature = {
+      val strTosig = s"GET\n\n$expireStr\n/$bucket/$key"
+      new HmacSHA1().sign(privateKey, strTosig)
+    }
+    s"$url/${encodeUrl(key)}?UCloudPublicKey=${encodeUrl(publicKey)}&Expires=${encodeUrl(expireStr)}&Signature=${encodeUrl(signature)}"
+  }
+
+  def privateDownloadFile(
+      key: String,
+      expire: Int = defaultExpireTime,
+      userAgent: String = "scalaj4",
+      url: String = requestUrl
+  ) = {
+    Http(privateDownloadFileUrl(key, expire, userAgent, url))
+      .header("User-Agent", userAgent)
+      .asBytes
   }
 }
